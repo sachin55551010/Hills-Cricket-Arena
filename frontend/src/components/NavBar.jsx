d66cbbe3c7080aff1c5a84f4e8846cae820f2779
sachin55551010 <sachinbansal3333@gmail.com>
2026-08-26 22:21:14 +0530

    fixed navbar logo redirect link

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,8 +1,8 @@
-import { X, Menu } from "lucide-react";
+import { Menu } from "lucide-react";
 import { SideMenuBar } from "./SideMenuBar";
 import { useDispatch, useSelector } from "react-redux";
 import { setIsMenuOpen } from "../store/authSlice";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export const NavBar = () => {
   const { isMenuOpen } = useSelector((state) => state.auth);
@@ -13,21 +13,22 @@ export const NavBar = () => {
       <header
         className={`fixed z-90 w-full h-[var(--nav-h)] flex items-center  px-2 justify-between bg-base-100 shadow-[0px_0px_10px_rgba(0,0,0,.4)]`}
       >
-        <div className="flex gap-2 items-center">
-          {!isMenuOpen && (
-            <Menu
-              strokeWidth={4}
-              onClick={() => dispatch(setIsMenuOpen(true))}
-              className={`ml-2 lg:hidden`}
-            />
-          )}
+        <Link to="/">
           <div className="flex gap-2 items-center">
-            <NavLink to="/" className="font-bold">
+            {!isMenuOpen && (
+              <Menu
+                strokeWidth={4}
+                onClick={() => dispatch(setIsMenuOpen(true))}
+                className={`ml-2 lg:hidden`}
+              />
+            )}
+            <div className="flex gap-2 items-center">
               <img src="/my_app_logo.png" alt="" className="h-8" />
-            </NavLink>
-            <h1 className="font-extrabold">Hills Cricket Arena</h1>
+
+              <h1 className="font-extrabold">Hills Cricket Arena</h1>
+            </div>
           </div>
-        </div>
+        </Link>
 
         <div className="lg:flex">
           <SideMenuBar />
