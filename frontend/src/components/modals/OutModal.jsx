3edae9c91f1d4a377f87e288c440e3964e4e24f1
sachin55551010 <sachinbansal3333@gmail.com>
2026-09-12 20:18:30 +0530

    back button added in extra run out modal

--- frontend/src/components/modals/OutModal.jsx
+++ frontend/src/components/modals/OutModal.jsx
@@ -25,6 +25,7 @@ const playerNameSchema = z
 export const OutModal = ({ pendingData = null, onClose, onSubmit }) => {
   const { currentMatchData } = useSelector((state) => state.score);
 
+
   const allWicketTypes = [
     { type: "Bowled" },
     { type: "Caught" },
