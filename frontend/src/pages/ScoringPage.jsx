2a114f66345fa6df9570919fa9aff1063b723804
Sachin <sachinbansal3333@gmail.com>
2026-09-15 17:01:54 +0530

    comment pending code

--- frontend/src/pages/ScoringPage.jsx
+++ frontend/src/pages/ScoringPage.jsx
@@ -11,12 +11,12 @@ import { useDispatch, useSelector } from "react-redux";
 import { recordDelivery } from "../store/scoreSlice";
 import { persistScoreState } from "../store/persistMatch";
 import { GrScorecard } from "react-icons/gr";
-import { useGetManOfTheMatchMutation } from "../store/matchApi";
+// import { useGetManOfTheMatchMutation } from "../store/matchApi";
 
 export const ScoringPage = () => {
   const scoreState = useSelector((state) => state.score);
-  const [getManOfTheMatch, { data, isLoading, error }] =
-    useGetManOfTheMatchMutation();
+  // const [getManOfTheMatch, { data, isLoading, error }] =
+  //   useGetManOfTheMatchMutation();
   const { currentMatchData } = scoreState;
   const [isExtraModalOpen, setIsExtraModalOpen] = useState(false);
   const [extraType, setExtraType] = useState("");
@@ -246,11 +246,11 @@ export const ScoringPage = () => {
     OUT: "border-3 border-red-600 text-red-600 text-[.8rem]",
   };
 
-  const handleTestBtn = () => {
-    const data = currentMatchData;
-    const matchId = currentMatchData.matchId;
-    getManOfTheMatch({ data, matchId });
-  };
+  // const handleTestBtn = () => {
+  //   const data = currentMatchData;
+  //   const matchId = currentMatchData.matchId;
+  //   getManOfTheMatch({ data, matchId });
+  // };
 
   return (
     <div className="h-dvh w-screen pt-12 flex justify-center">
@@ -583,9 +583,6 @@ export const ScoringPage = () => {
           onCancel={() => setShowUndoConfirmModal(false)}
         />
       )}
-      <button onClick={handleTestBtn} className="btn btn-dash">
-        testing
-      </button>
     </div>
   );
 };
